30618eb8745c457b5f3f67c5737f6f61ee8b571f
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-03 17:31:31 +0100

    lost

---
+++ sript.js
@@ -0,0 +1,8 @@
+const logo = document.querySelector(".contain");
+const menu = document.querySelector(".menu");
+
+logo.addEventListener("click", function () {
+  menu.classList.toggle("showmenu");
+});
+
+
