bbcd833ce2767828920d3733e250eb7bbc24065e
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-03 15:43:16 +0100

    mob responsiveness

--- sript.js
+++
@@ -1,9 +0,0 @@
-const mob = document.querySelector(".topnav");
-
-function run() {
-  mob.classList.toggle("dark");
-}
-
-// document.querySelector("#hamb").onclick = run;
-
-mob.addEventListener('click', run)
